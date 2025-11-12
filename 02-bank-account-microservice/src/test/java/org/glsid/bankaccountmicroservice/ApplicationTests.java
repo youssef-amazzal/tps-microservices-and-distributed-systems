3283b89e8d0a4ca638e747ffa585908f7eef1fa7
Youssef Amazzal <20youssefyoussef02@gmail.com>
2025-11-12 18:08:30 +0100

    feat(02): initialize bank account microservice project

---
+++ 02-bank-account-microservice/src/test/java/org/glsid/bankaccountmicroservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.glsid.bankaccountmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
